aa86fb8528178c6f2dd00da6442792da7ab6c1ca
isamaulanatantra <isamaulanatantra@gmail.com>
2024-03-26 08:47:10 +0700

    controller data keluarga

---
+++ resources/views/back/pages/datakeluarga/index.blade.php
@@ -0,0 +1,15 @@
+@extends('back.layouts.app')
+@section('content')
+<!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <!-- row -->
+			<div class="container-fluid">
+
+            </div>
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\DataKeluargaController;
 use App\Http\Controllers\ProfileController;
 use App\Livewire\Front\Home;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('dkeluarga', DataKeluargaController::class);
 });
 
 require __DIR__ . '/auth.php';
